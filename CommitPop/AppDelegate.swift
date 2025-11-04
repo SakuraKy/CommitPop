9e05b54d4ddcf2c0759c05d0ce76744cda1281c7
SakuraKy <2790409858@qq.com>
2025-11-04 12:31:50 +0800

    Initial Commit

---
+++ CommitPop/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  CommitPop
+//
+//  Created by 沈柯宇 on 2025/11/4.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ CommitPop/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CommitPop
+//
+//  Created by 沈柯宇 on 2025/11/4.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
